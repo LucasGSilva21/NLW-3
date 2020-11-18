621798e234dded0ff6cb09c4461c7d7b2052b9f0
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2020-11-18 15:40:18 -0300

    criando link da landing para orphanagesMap

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi'
+import { FiArrowRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import '../styles/pages/landing.css';
 
@@ -21,9 +22,9 @@ function Landing() {
                 <span>Minas Gerais</span>
                 </div>
 
-                <a href="" className="enter-app" >
+                <Link to="/app" className="enter-app" >
                 <FiArrowRight size={24} color="rgba(0, 0, 0, 0.6)" />
-                </a>
+                </Link>
             </div>
         </div>
     );
