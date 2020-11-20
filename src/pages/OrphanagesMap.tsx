7276ab369bf7b7640412ed20803844aeb200a48a
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2020-11-20 18:25:47 -0300

    criando pagina com mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css';
+
+import iconMap from '../images/iconMap.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
     return (
-        <h1>OrphanagesMap</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={iconMap} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando</p>
+                </header>
+
+                <footer>
+                    <strong>Divinópolis</strong>
+                    <strong>Minas Gerais</strong>
+                </footer>
+            </aside>
+
+            <MapContainer 
+                center={[-20.1660855,-44.8786074]}
+                zoom={15}
+                style={{
+                    width: '100%',
+                    height: '100%'
+                }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </MapContainer>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+        </div>
     );
 }
 
